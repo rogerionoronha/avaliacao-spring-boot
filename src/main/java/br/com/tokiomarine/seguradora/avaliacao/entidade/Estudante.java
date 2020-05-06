d0b67dc17203edd95a1ff6b3340b264f9322c0dc
Rogerio Noronha <rogerio.noronha@gmail.com>
2020-05-05 23:56:00 -0300

    - Correção da propriedade de "nome" para "name" nas paginas; - Alteração do campo "Adicionar Estudante" para "submit"; - Tratamento do tipo/cor do botão Editar e Apagar;

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,35 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="Estudante")
 public class Estudante {
 	// TODO Implementar a entidade Estudante conforme solicitado
+
+
+	@Id
+	@SequenceGenerator(name="estudante_seq", sequenceName="estudante_seq", allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="estudante_seq")
+	private Long id;
+	
+	@NotNull(message = "Nome é obrigatório")
+	@Column(name = "nome")
+	private String name;
+	
+	@NotNull(message = "Email é obrigatório")
+	private String email;
+	
+	private String telefone;
+	
 }
