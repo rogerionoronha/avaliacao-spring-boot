a3bd74c4ad4644f7a3b1ab3cc5698cbe9bc97095
Rogerio Noronha <rogerio.noronha@gmail.com>
2020-05-06 01:08:36 -0300

    - Inclusão dos campos "matricula" e "curso" na classe Estudante; - Inclusão dos campos "matricula" e "curso" nas paginas; - Anotação de campos obrigatórios e mensagens;

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -24,12 +25,19 @@ public class Estudante {
 	private Long id;
 	
 	@NotNull(message = "Nome é obrigatório")
+	@NotEmpty(message = "Nome é obrigatório")
 	@Column(name = "nome")
 	private String name;
 	
 	@NotNull(message = "Email é obrigatório")
+	@NotEmpty(message = "Email é obrigatório")
 	private String email;
 	
+	@NotNull(message = "Matrícula é obrigatória")
+	@NotEmpty(message = "Matrícula é obrigatória")
+	private String matricula;
+	
 	private String telefone;
+	private String curso;
 	
 }
